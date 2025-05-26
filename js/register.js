8d73d885fa47e813944c718519ce6e5530b94187
Sufyan Malik <sufyaanmaalik@gmail.com>
2025-05-26 11:05:07 +0500

    Update Code

--- js/register.js
+++ js/register.js
@@ -1,26 +1,26 @@
 import { auth, createUserWithEmailAndPassword } from "./firebase.js";
 
 let signUp = (e) => {
-    e.preventDefault();
+e.preventDefault()
 
-    let email = document.getElementById("email");
-    let password = document.getElementById("password");
+let email = document.getElementById("email");
+let password = document.getElementById("password");
 
-    createUserWithEmailAndPassword(auth, email.value, password.value)
-        .then((userCredential) => {
-            // Signed up 
-            const user = userCredential.user;
-            console.log(user)
-            // ...
-        })
-        .catch((error) => {
-            const errorCode = error.code;
-            const errorMessage = error.message;
-            console.log(errorMessage);
-            // ..
-        });
+createUserWithEmailAndPassword(auth, email.value, password.value)
+  .then((userCredential) => {
+    // Signed up 
+    const user = userCredential.user;
+        console.log(user)
+    // ...
+  })
+  .catch((error) => {
+    const errorCode = error.code;
+    const errorMessage = error.message;
+        console.log(errorMessage);
+    // ..
+  });
 
 }
 
-let signUpForm = document.querySelector("form");
+let signUpForm = document.getElementById("signUpForm");
 signUpForm.addEventListener("submit", signUp);
